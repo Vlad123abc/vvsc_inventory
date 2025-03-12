dd4a1c4ec465a9e727e6cf22a5bf325ceba7f89c
Vlad123abc <vladi.ivanescu@gmail.com>
2025-03-12 13:18:21 +0200

    add Lab01_ReviewReport.xlsx

--- src/main/java/inventory/repository/InventoryRepository.java
+++ src/main/java/inventory/repository/InventoryRepository.java
@@ -21,16 +21,13 @@ public class InventoryRepository {
 		//ClassLoader classLoader = InventoryRepository.class.getClassLoader();
 		File file = new File(filename);
 		ObservableList<Part> listP = FXCollections.observableArrayList();
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader(file));
+		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
 			String line = null;
 			while((line=br.readLine())!=null){
 				Part part=getPartFromString(line);
 				if (part!=null)
 					listP.add(part);
 			}
-			br.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -139,16 +136,16 @@ public class InventoryRepository {
 			}
 
 			for (Product pr:products) {
-				String line=pr.toString()+",";
+				StringBuilder line= new StringBuilder(pr.toString() + ",");
 				ObservableList<Part> list= pr.getAssociatedParts();
 				int index=0;
 				while(index<list.size()-1){
-					line=line+list.get(index).getPartId()+":";
+					line.append(list.get(index).getPartId()).append(":");
 					index++;
 				}
 				if (index==list.size()-1)
-					line=line+list.get(index).getPartId();
-				bw.write(line);
+					line.append(list.get(index).getPartId());
+				bw.write(line.toString());
 				bw.newLine();
 			}
 			bw.close();
