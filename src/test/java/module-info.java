9ee7d51874795403ec2874029bad19ab01fed93a
Iosub lucian <iosublucian9@gmail.com>
2025-05-16 10:28:17 +0300

    Lab4

---
+++ src/test/java/module-info.java
@@ -0,0 +1,13 @@
+module inventory {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+
+    opens inventory.model to javafx.base;
+    exports inventory.model;
+    opens inventory to javafx.fxml;
+    exports inventory;
+    opens inventory.controller to javafx.fxml;
+    exports inventory.controller;
+    opens inventory to ALL-UNNAMED;
+}
